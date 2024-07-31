6ebcde230fe5f564235daef29c020b704a6a91e6
Brawijayadewa <Orlandoryan01@gmail.com>
2024-07-31 10:26:05 +0700

    initial commite

---
+++ resources/views/bootstrap.blade.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container-fluid">
+      <div class="container">
+        <h1>TEST</h1>
+      <h1>Hello World!</h1>
+      <p>This Is Bootstrap Test.</p>
+      <div class="container">          
+        <table class="table table-striped">  
+          <thead>
+            <tr>
+              <th>Firstname</th>
+              <th>Lastname</th>
+              <th>Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Orlando</td>
+              <td>Natasha</td>
+              <td>orlandoryan@gmail.com</td>
+            </tr>
+            <tr>
+              <td>Ryan</td>
+              <td>Campbell</td>
+              <td>natashacampbell@gmail.com</td>
+            </tr>
+            <tr>
+              <td>Charlotte</td>
+              <td>Mikan</td>
+              <td>charlottemikant@gmail.com</td
+            </tr>
+            <div class="alert alert-info">
+              <strong>Info!</strong> This List N.
+            </div>
+          </tbody>
+        </table>
+      </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
